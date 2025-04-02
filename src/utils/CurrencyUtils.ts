18db05e4180d24ca7c94104da27259af17db82b3
Shizhan Liu <87243352+ShizhanLiu@users.noreply.github.com>
2025-04-01 23:55:01 -0400

    usd

--- src/utils/CurrencyUtils.ts
+++ src/utils/CurrencyUtils.ts
@@ -2,7 +2,7 @@ class CurrencyUtils {
     static formatToINR(amount: number) {
         return new Intl.NumberFormat('en-IN', {
             style: 'currency',
-            currency: 'INR'
+            currency: 'USD'
         }).format(amount);
     }
 }
