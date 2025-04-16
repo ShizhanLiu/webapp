69444482206030c40a107d67a4a4dbbb6027f751
dix <zhao.dix@northeastern.edu>
2025-04-16 12:42:38 -0700

    complete registration

---
+++ src/services/auth-service.ts
@@ -0,0 +1,5 @@
+import apiClient from "../config/api-client";
+import { Profile } from "../model/Profile";
+export const createProfile = (profile: Profile) => {
+    return apiClient.post<Profile>("/register",profile);
+}
