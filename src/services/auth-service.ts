88cdd438df80f36fa59ddc2f2b0bea8e1e14f49b
dix <zhao.dix@northeastern.edu>
2025-04-19 22:47:24 -0700

    Add logout logic: Navbar, useSignout hook, and auth-service

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -1,5 +1,16 @@
 import apiClient from "../config/api-client";
+import { AuthRequest } from "../model/AuthRequest";
 import { Profile } from "../model/Profile";
+import { AuthResponse } from "../model/AuthResponse";
+
 export const createProfile = (profile: Profile) => {
     return apiClient.post<Profile>("/register",profile);
+}
+
+export const authenticate = (authRequest: AuthRequest) => {
+    return apiClient.post<AuthResponse>("/login", authRequest);
+}
+
+export const signout = () => {
+    return apiClient.post<void>(".signout");
 }
