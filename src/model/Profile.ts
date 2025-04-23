c3885dd6bd2a085044cec8dbf5a25232ce741d9f
Shizhan Liu <87243352+ShizhanLiu@users.noreply.github.com>
2025-04-23 14:43:32 +0800

    Revert "Merge branch 'main' of https://github.com/ShizhanLiu/webapp"
    This reverts commit 825cb084965a92f875e240826fdb8cc8007b305a, reversing
    changes made to 347136caa3a2e8255b3895006affad8e68f130fc.
    
    hanges. Lines starting

--- src/model/Profile.ts
+++
@@ -1,7 +0,0 @@
-export interface Profile {
-    profileId?: string;
-    name: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-}
--- src/services/auth-service.ts
+++
@@ -1,5 +0,0 @@
-import apiClient from "../config/api-client";
-import { Profile } from "../model/Profile";
-export const createProfile = (profile: Profile) => {
-    return apiClient.post<Profile>("/register",profile);
-}
